0fb47254c32121a08d121bbaeb52d64f8bc4775f
Paul Gierz <pgierz@awi.de>
2020-03-24 11:47:34 +0100

    Bump version: 0.1.0 → 3.0.0

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitlab.awi.de/esm_tools/esm_parser',
-    version='0.1.0',
+    version='3.0.0',
     zip_safe=False,
 )
