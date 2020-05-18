9066a415de9bf9b1d67a017ff2619334d91d0a05
Paul Gierz <pgierz@awi.de>
2020-05-18 10:39:30 +0200

    Bump version: 4.0.1 → 4.0.2

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/esm-tools/esm_parser',
-    version="4.0.1",
+    version="4.0.2",
     zip_safe=False,
 )
