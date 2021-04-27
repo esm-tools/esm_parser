e7bfc386e0bed5ed37dd66e03697bdff1dcb248f
Miguel Andres-Martinez <miguel.andres-martinez@awi.de>
2021-04-27 20:53:41 +0200

    Bump version: 5.0.9 → 5.1.0

--- setup.py
+++ setup.py
@@ -54,6 +54,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitlab.awi.de/esm_tools/esm_parser',
-    version="5.0.9",
+    version="5.1.0",
     zip_safe=False,
 )
