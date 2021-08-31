86b1e82944d13d0778e11e2765fc21376a6b46f1
denizural <denizural86@gmail.com>
2021-08-31 14:11:22 +0200

    Bump version: 5.1.9 → 5.1.10

--- setup.py
+++ setup.py
@@ -54,6 +54,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitlab.awi.de/esm_tools/esm_parser',
-    version="5.1.9",
+    version="5.1.10",
     zip_safe=False,
 )
